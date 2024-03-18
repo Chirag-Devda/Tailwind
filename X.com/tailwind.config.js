00d7c0a4f72fe2c98420dc8ce90c81c70e8a7da8
Chirag-Devda <devdachirag09@gmail.com>
2024-03-18 21:11:55 +0530

    initialize

---
+++ X.com/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      "baloo-bhaina-2": ['"Baloo Bhaina 2"', "sans-serif"],
+      "league-spartan": ['"League Spartan"', "sans-serif"],
+      oxanium: ['"Oxanium"', "sans-serif"],
+    },
+  },
+  plugins: [],
+};
